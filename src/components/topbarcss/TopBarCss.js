e0197977f85b3483fa6463e2d3513783ff51ef92
Amir <ahossein960@gmail.com>
2023-09-13 11:26:05 +0330

    Top Bar Css is Done

---
+++ src/components/topbarcss/TopBarCss.js
@@ -0,0 +1,61 @@
+import {Grid, Typography} from "@mui/material";
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import ContactPhoneOutlinedIcon from '@mui/icons-material/ContactPhoneOutlined';
+import ForwardToInboxOutlinedIcon from '@mui/icons-material/ForwardToInboxOutlined';
+function TopBarCss(){
+    return(
+        <>
+            <Grid container xs={12} sx={{backgroundColor:"#fdbe33" , position:"relative" , p: 3}}>
+
+                <Grid item xs={12} sm={12} md={6} lg={6} xl={6} sx={{textAlign:{xs:"center" , sm:"center" , md:"0"}, }}>
+                    <Typography variant="h1" fontFamily="Lalezar" fontWeight="1.5">
+                        سازندگان
+                    </Typography>
+                </Grid>
+
+                <Grid container xs={4} sm={4} md={5} lg={6} xl={6} sx={{display:{xs:"none" , sm:"none" , md:"flex" },fontSize:{md:"25px" , lg:"20px"}}}>
+
+
+                    <Grid item xs={4} >
+                        <CalendarMonthOutlinedIcon fontSize="large"/>
+
+                        <Typography fontFamily="Lalezar" >
+                            آدرس
+                        </Typography>
+
+                        <Typography fontFamily="Lalezar">
+                            زنجان , اعتمادیه
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                        <ContactPhoneOutlinedIcon fontSize="large"/>
+                        <Typography fontFamily="Lalezar">
+                            تماس با ما
+                        </Typography>
+
+                        <Typography fontFamily="Lalezar">
+                            0243332265
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                        <ForwardToInboxOutlinedIcon fontSize="large"/>
+                        <Typography fontFamily="Lalezar">
+                            ایمیل ما
+                        </Typography >
+
+                        <Typography fontFamily="Lalezar">
+                            info@gmail.com
+                        </Typography>
+                    </Grid>
+
+
+                </Grid>
+
+            </Grid>
+        </>
+    )
+}
+
+export default TopBarCss
