1a3b8030abfc2ad9f0872e485cd05b124682a406
emad <arabanemad@gmail.com>
2023-09-13 18:06:04 +0330

    Update Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,32 @@
 import React from "react";
 import TopBarCss from "../Components/topbarcss/TopBarCss";
 import RightBar from "../Components/RightBar/RightBar";
+import BtnAnimation from "../Components/BtnAnimation/BtnAnimation";
+import OurService from "../Components/OurService/OurService";
+import { Container, Grid } from "@mui/material";
 
 const Home = () => {
   return (
     <>
       <TopBarCss />
-      <RightBar />
+      <Grid container>
+        <Grid xs={2}>
+          <RightBar />
+        </Grid>
+
+        <Grid
+          xs={10}
+          md={6}
+          sx={{
+            maxWidth: { md: "1200px" },
+            pr: { xs: 5, sm: 10, md: 5, lg: 22 },
+
+            pl: { xs: 5, sm: 10, md: 0 },
+          }}
+        >
+          <OurService />
+        </Grid>
+      </Grid>
     </>
   );
 };
