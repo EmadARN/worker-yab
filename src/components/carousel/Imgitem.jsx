69c547b83d506cede5075a0a8af5961d4458dc38
Reza <rezafafaa78@gmail.com>
2023-09-14 14:15:11 +0330

    contact us has been maeded!

--- src/components/carousel/Imgitem.jsx
+++ src/components/carousel/Imgitem.jsx
@@ -16,8 +16,8 @@ const Imgitem = (props) => {
         /> 
 
 <Box sx={{position:"absolute",display:'flex',justifyContent:'center',flexDirection:"column",alignItems:"center",top:"0",bottom:"0",width: "calc(100vw - 20px)"}}>
-<Typography className="animate__animated animate__backInRight" sx={{color:'#fff',fontWeight:"bold",textAlign:"center",fontSize:{xs:"28px",md:"35px"}}} variant='h4'>{props.item.text}</Typography>
-<Typography className='animate__animated animate__backInLeft' sx={{color:"#fff",fontWeight:"bold",textAlign:"center",fontSize:{xs:"28px",md:"45px"}}} variant='h2'>{props.item.title}</Typography>
+<Typography className="animate__animated animate__backInRight" sx={{color:'#fff',fontWeight:"bold",textAlign:"center",fontSize:{xs:"24px",md:"35px"}}} variant='h4'>{props.item.text}</Typography>
+<Typography className='animate__animated animate__backInLeft' sx={{color:"#fff",fontWeight:"bold",textAlign:"center",fontSize:{xs:"24px",md:"45px"}}} variant='h2'>{props.item.title}</Typography>
 <Box sx={{"@media (max-width:770px)":{position:"absolute",bottom:"25%"},display:{xs:"none",sm:"flex"}}}>
   <Button sx={{top:"90%",border:"1px solid white",padding:"10px 40px",borderRadius:"7px",color:"white",fontWeight:"bold",cursor:"pointer","&:hover":{backgroundColor:"gray",}}}>دریافت مشاوره</Button>
 </Box>
