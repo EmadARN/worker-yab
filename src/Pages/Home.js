dcd7e1e25dbdcbfa94129454da14b9d83c2f621b
emad <arabanemad@gmail.com>
2023-09-14 11:23:36 +0330

    Commit for Fixed Grid Component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,7 @@ import RightBar from "../Components/RightBar/RightBar";
 import BtnAnimation from "../Components/BtnAnimation/BtnAnimation";
 import OurService from "../Components/OurService/OurService";
 import { Container, Grid } from "@mui/material";
+import Carousel1 from "../Components/carousel/Carousel";
 
 const Home = () => {
   return (
@@ -13,8 +14,34 @@ const Home = () => {
         <Grid xs={2}>
           <RightBar />
         </Grid>
-
-        <Grid
+        <Grid xs={10} md={12}>
+          <Carousel1
+            describtion={[
+              {
+                id: 1,
+                text: "کارگران حرفه ای",
+                title: "خانه رویایی خود رت به ما بسپرید",
+                image:
+                  "https://initiafy-website-images.s3.amazonaws.com/wordpress-upload/2015/06/Canada-New-Worker-Safety-group-of-workers.jpg",
+              },
+              {
+                id: 2,
+                text: "ماقابل اعتماد هستیم",
+                title: "خانه ی شما را میسازیم",
+                image:
+                  "https://initiafy-website-images.s3.amazonaws.com/wordpress-upload/2015/06/Canada-New-Worker-Safety-group-of-workers.jpg",
+              },
+              {
+                id: 3,
+                text: "سازندگان حرفه ای",
+                title: "پروژه های خود را به ما بسپارید",
+                image:
+                  "https://initiafy-website-images.s3.amazonaws.com/wordpress-upload/2015/06/Canada-New-Worker-Safety-group-of-workers.jpg",
+              },
+            ]}
+          />
+        </Grid>
+        {/* <Grid
           xs={10}
           md={6}
           sx={{
@@ -25,7 +52,7 @@ const Home = () => {
           }}
         >
           <OurService />
-        </Grid>
+        </Grid> */}
       </Grid>
     </>
   );
