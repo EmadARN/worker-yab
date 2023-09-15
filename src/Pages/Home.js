1a3416c5eb353c3c469ad880e0f751c43f819698
emad <arabanemad@gmail.com>
2023-09-16 01:53:04 +0330

    Update Home & Table

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,8 @@ import ImageSlider from "../components/carousel/ImageSlider";
 import Fact from "../components/Fact/Fact";
 import About from "../components/About/About";
 import FAQs from "../components/faqs/FAQs";
-import {Footer} from '../components/Footer/Footer'
+import { Footer } from "../components/Footer/Footer";
+import TableMain from "../components/Table/TableMain";
 
 const Home = () => {
   return (
@@ -19,7 +20,7 @@ const Home = () => {
           <RightBar />
         </Grid>
         <Grid container xs={10} md={12}>
-          <Grid xs={12} sx={{height:"20%"}}>
+          <Grid xs={12} sx={{ height: "20%" }}>
             <ImageSlider />
           </Grid>
           <Grid xs={12} sx={{ mt: 10 }}>
@@ -28,25 +29,29 @@ const Home = () => {
           <Grid xs={12} sx={{ mt: 10 }}>
             <Fact scroll="1500" />
           </Grid>
+          <Grid xs={10} md={8} sx={{ margin: "150px auto" }}>
+            <TableMain />
+          </Grid>
           <Grid xs={10} md={8} sx={{ margin: "70px auto" }}>
             <OurService />
           </Grid>
-          <Grid xs={10} md={8} sx={{ margin: " auto" }}>
+          <Grid xs={10} md={8} sx={{ margin: "150px auto" }}>
             <FAQs />
           </Grid>
-        </Grid>
 
-        <Footer
-         page1 = {"درباره ما"}
-         page2={"ارتباط با ما"}
-         page3={"خدمات"}
-         services1={"خدمات مکانیکی"}
-         services2={"کارگران ساختمانی"}
-         address={'اعتمادیه.خیابان اول عربی'}
-         phNumber={"0919123456"}
-         email={"test1@gmail.com"}
- 
-        />
+          <Grid xs={10} sx={{}}>
+            <Footer
+              page1={"درباره ما"}
+              page2={"ارتباط با ما"}
+              page3={"خدمات"}
+              services1={"خدمات مکانیکی"}
+              services2={"کارگران ساختمانی"}
+              address={"اعتمادیه.خیابان اول عربی"}
+              phNumber={"0919123456"}
+              email={"test1@gmail.com"}
+            />
+          </Grid>
+        </Grid>
       </Grid>
     </>
   );
