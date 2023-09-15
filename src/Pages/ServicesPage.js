1231c96064dbdf89928d1894c9103ed5ab45fc05
emad <arabanemad@gmail.com>
2023-09-15 16:10:18 +0330

    Create ServicesPage

---
+++ src/Pages/ServicesPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import TopBarCss from "../components/topbarcss/TopBarCss";
+import RightBar from "../components/RightBar/RightBar";
+import OurService from "../components/OurService/OurService";
+import { Grid } from "@mui/material";
+import ImageSlider from "../components/carousel/ImageSlider";
+import Fact from "../components/Fact/Fact";
+import About from "../components/About/About";
+import FAQs from "../components/faqs/FAQs";
+
+const ServicesPage = () => {
+  return (
+    <>
+      <TopBarCss />
+      <Grid container>
+        <Grid xs={2}>
+          <RightBar />
+        </Grid>
+        <Grid container xs={10} md={12}>
+          <Grid xs={10} md={8} sx={{ margin: "70px auto" }}>
+            <OurService />
+          </Grid>
+          <Grid xs={10} md={8} sx={{ margin: " auto" }}>
+            <FAQs />
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default ServicesPage;
