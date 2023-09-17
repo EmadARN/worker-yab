5a8eb7b9541445e0f40b5b455e38e44ce70be890
Amir <ahossein960@gmail.com>
2023-09-17 18:51:02 +0330

    About img & About Page.js response is Done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AboutTitle from "./about title/About Title";
 import { Grid } from "@mui/material";
 import AboutImg from "./about img/About Img";
-import img1 from "../../Asset/image/about.jpg";
+
 const About = () => {
   return (
     <Grid container xs={12} sx={{mt:{xs:'12%',md:"10%"}}}>
@@ -16,9 +16,9 @@ const About = () => {
         md={5}
         lg={5}
         xl={5}
-        sx={{ mr: { xs: "40px", sm: "40px", md: "0", lg: "0", xl: "0" } }}
+        sx={{ mr: { xs: "400px", sm: "40px", md: "0", lg: "0", xl: "0" }}}
       >
-        <AboutImg imgs1={img1} />
+        <AboutImg />
       </Grid>
     </Grid>
   );
