c85e83729259e420b993ebd93df6d1aab9057bad
emad <arabanemad@gmail.com>
2023-09-18 11:08:07 +0330

    Update VerifyNumber

--- src/components/SignUpItems/SignUp(VerifyCode)/VerifyCode.jsx
+++ src/components/SignUpItems/SignUp(VerifyCode)/VerifyCode.jsx
@@ -1,123 +1,59 @@
-import { Box, Button, Grid, TextField, Typography } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
-
-const ButtonStyle = {
-  textAlign: "right",
-  marginRight: "10%",
-  fontFamily: "Yekan",
-  fontWeight: "bold",
-  fontSize: "20px",
-};
-
-const MainGrid = {
-  display: "felx",
-  justifyContent: "center",
-  height: "100vh",
-  alignItems: "center",
-};
-
-const InnerGrid = {
-  border: "1px solid gray",
-  width: "25%",
-  backgroundColor: "#fdbe33",
-  height: "450px",
-
-  borderRadius: "7px",
-};
-
-const ConfirmbtnBox={
-  marginTop: "10%",
-  display: "flex",
-  justifyContent: "center",
-  textAlign: "center",
-  width: "90%",
-  marginRight: "4%",
-  backgroundColor: "#030f27",
-}
-
-const VerifyCode = (props) => {
-
-const [value,setValue]=useState([])
-
-const InputRef=useRef()
-
-
-
-  function myFunc(event){
-if(setValue(event.key) == 'Backspace'){
- setValue(value.pop()) ;
-
-}
-  for(var i =1 ; i<=value.length;i++){
-  setValue(value+= "_");
-  setValue(value.shift());
-  
-  }
-  console.log(value);
-  }
-
+import React from "react";
+import Title from "../../Title/Title";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Grid, ThemeProvider, createTheme } from "@mui/material";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import Counter from "lyef-counter";
+import { prefixer } from "stylis";
+const theme = createTheme({
+  direction: "rtl", // Both here and <body dir="rtl">
+});
+// Create rtl cache
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+const VerifyNumber = () => {
   return (
     <>
-      <Grid container style={MainGrid}>
-        <Grid style={InnerGrid}>
-          <Typography
-            fontWeight="bold"
-            fontSize="25px"
-            color="##030f27"
-            sx={{ textAlign: "center", marginTop: "12%" }}
+      <Title title="کد  را وارد کنید" />
+      <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+          <Grid
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              mt: 20,
+              "& .MuiInput-underline:after": {
+                borderBottomColor: "#fdbe33",
+              },
+              "& label.Mui-focused": {
+                color: "#030f27",
+                fontWeight: "bold",
+                fontSize: "22px",
+              },
+            }}
           >
-            کد تایید را وارد کنید
-          </Typography>
-          <Box sx={{ textAlign: "center", margin: "6% 0 12% 0" }}>
-            <Typography color="#030f27" fontWeight="bold" fontSize="18px">
-              کد تایید برای شماره {props.number}ارسال شد
-            </Typography>
-
-            <Box sx={{ mt: "10%" }}>
-              <TextField
-              onKeyDown={(e) => myFunc(e)}
-              value={value}
-            ref={InputRef}
-              onChange={(e)=>setValue(e.target.value)}
-                sx={{
-                  width: "80%",
-                  borderRadius: "7px",
-                  input: { border: "3px solid #030f2 !important" },
-
-                  "&:hover fieldset": {
-                    border: "3px solid black !important",
-                  },
-                }}
-                variant="outlined"
-              />
-            </Box>
-          </Box>
-          <Box>
-            <Button
+            <TextField
+              id="standard-basic"
+              label=" کد را وارد کنید"
+              variant="filled"
               sx={{
-                "&:hover": {
-                  color: "gray",
+                "& .MuiFilledInput-underline:after": {
+                  borderBottomColor: "#fdbe33",
                 },
-                color: " black",
-               
-              }}
-              style={ButtonStyle}
-            >
-              ورود با رمز عبور
-            </Button>
-          </Box>
 
-          <Box
-         style={ConfirmbtnBox}
-          >
-            <Button sx={{ color: "white", padding: "8px 0", fontSize: "20px" }}>
-              تایید
-            </Button>
-          </Box>
-        </Grid>
-      </Grid>
+                width: { xs: "80%", lg: "50%", xl: "30%" },
+              }}
+            />
+          </Grid>
+        </ThemeProvider>
+      </CacheProvider>
     </>
   );
 };
 
-export default VerifyCode;
+export default VerifyNumber;
