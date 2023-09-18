3ff155cbf3ea3843983d658f7e38cb0bdc5b8424
Amir <ahossein960@gmail.com>
2023-09-18 10:27:59 +0330

    SignUpForm.jsx & SignUpImage.jsx SignUpPage2.js is Done

--- src/components/SignUpItems/SignUpImage/SignUpImage.jsx
+++ src/components/SignUpItems/SignUpImage/SignUpImage.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
-import rtlPlugin from "stylis-plugin-rtl";
-import {createTheme, Grid} from "@mui/material";
-import createCache from "@emotion/cache/dist/emotion-cache.cjs";
-import {prefixer} from "stylis";
-
-const theme = createTheme({
-  direction: "rtl"
-});
-
-const cacheRtl = createCache({
-  key: "muirtl",
-  stylisPlugins: [prefixer, rtlPlugin],
-});
+import {Grid} from "@mui/material";
 
 const SignUpImage = () => {
   return (
     <>
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-          <img src=".././Asset/image/Sign.jpg" />
+          <img src={"https://cdni.iconscout.com/illustration/premium/thumb/sign-up-6333618-5230178.png"} style={{ width:"100%" , height:"auto"}}/>
+
       </Grid>
     </>
   )
